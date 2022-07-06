c13dccbef75ee6fdef7145aa34887d382ba87b4b
Satya <reach.satya09@gmail.com>
2022-07-06 13:00:07 +0530

    springbbot on docker

--- src/main/java/com/mkyong/docker/spring/boot/controller/HelloController.java
+++ src/main/java/com/mkyong/docker/spring/boot/controller/HelloController.java
@@ -3,8 +3,11 @@ package com.mkyong.docker.spring.boot.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/hello")
     public String getHello(){
